ff0df995ca96ed45274b565f5d6e03b79bbc78c8
sidechain-studio <studio@sidechain.studio>
2016-11-19 17:30:41 +1100

    testing

--- keystone.js
+++ keystone.js
@@ -36,11 +36,6 @@ keystone.init({
 	'user model': 'User',
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
-
 // Load your project's Models
 keystone.import('models');
 
